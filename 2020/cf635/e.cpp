e7fd72ac702b7b6603e7cab397727d19bcc4db7e
Ivan Chau <ivanmchau@gmail.com>
2020-04-24 20:29:40 -0400

    upsolve magic spell cf635

---
+++ 2020/cf635/e.cpp
@@ -0,0 +1,15 @@
+//#include "../../cpp/config/bits/stdc++.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+string s;
+string target;
+void solve() {
+   cin >> s >> target;
+}  
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+}
